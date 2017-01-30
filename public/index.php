d732332411723927a2031221db23e28655d51b4f
Lousy Lynx <mttprvstanddad@gmail.com>
2017-01-29 20:08:42 -0600

    Big update...

--- public/index.php
+++ public/index.php
@@ -10,10 +10,22 @@ $config['addContentLengthHeader'] = false;
 $app = new \Slim\App(['settings' => $config]);
 $container = $app->getContainer();
 
-$container['view'] = new \Slim\Views\PhpRenderer("../templates/");
+$container['view'] = function ($c) {
+    $view = new \Slim\Views\Twig('../templates');
+
+    // Instantiate and add Slim specific extension
+    $basePath = rtrim(str_ireplace('index.php', '', $c['request']->getUri()->getBasePath()), '/');
+    $view->addExtension(new Slim\Views\TwigExtension($c['router'], $basePath));
+
+    return $view;
+};
 
 $app->get('/', function (Request $request, Response $response) {
-    return $this->view->render($response, 'home.phtml');
+    return $this->view->render($response, 'home.html');
 });
+
+$app->get("/{page}", function (Request $request, Response $response) {
+    return $this->view->render($response, $request->getAttribute("page") . ".html");
+})->setName("page");
 
 $app->run();
